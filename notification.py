00d8309a8f298d7631be57045ecea200964fc881
fxteguhab <31503256+fxteguhab@users.noreply.github.com>
2018-08-15 13:50:09 +0700

    rapihin + edit line_str di invoice alert

--- notification.py
+++ notification.py
@@ -291,57 +291,23 @@ class account_invoice_line(osv.osv):
 		partner_id = context.get('partner_id','')
 
 		message_body = ''
-		
+		line_str = ''
+
 		if (price_unit_nett_old > 0) and (price_unit > 0) and (round(price_unit_nett_old) != round(price_unit_nett)):
 			if round(price_unit_old) != round(price_unit):
 				message_body += 'PLIST From '+ str("{:,.0f}".format(price_unit_old))+' to '+str("{:,.0f}".format(price_unit)) +'\n'
 			if discount_string_old != discount_string:
 				message_body += 'DISC From '+ str(discount_string_old)+' to '+ str(discount_string) +'\n'
 
-			message_body += 'NETT From '+ str("{:,.0f}".format(price_unit_nett_old))+' to '+str("{:,.0f}".format(price_unit_nett)) +'\n'	
+			line_str += 'NETT From '+ str("{:,.0f}".format(price_unit_nett_old))+' to '+str("{:,.0f}".format(price_unit_nett)) +'\n'	
+			message_body += line_str
 
 			#account_invoice_obj = self.pool.get('account.invoice')
 			#account_invoice = account_invoice_obj.browse(cr, uid, invoice_id)
 			#supplier_name = account_invoice.partner_id.display_name
 
-			message_body += 'SELL PRICE:'+str("{:,.0f}".format(sell_price_unit)) +'\n'+'Supplier:'+partner_name
-
-		#price_unit_nett_old = vals['price_unit_nett_old'] if 'price_unit_nett_old' in vals else 0
-		#price_unit_nett = vals['price_unit_nett'] if 'price_unit_nett' in vals else 0
-		
-		#discount_string_old = vals['discount_string_old'] if 'discount_string_old' in vals else "0"
-		#discount_string = vals['discount_string'] if 'discount_string' in vals else "0"
-		
-		#price_unit_old = vals['price_unit_old'] if 'price_unit_old' in vals else 0
-		#price_unit = vals['price_unit'] if 'price_unit' in vals else 0
-
-		#sell_price_unit = vals['sell_price_unit'] if 'sell_price_unit' in vals else 0
-		
-		#if (price_unit_nett_old > 0) and (price_unit_nett_old != price_unit_nett):
-		#	if (price_unit_old > price_unit):
-		#		message_body += 'PLIST DOWN From '+ str("{:,.0f}".format(price_unit_old))+' to '+str("{:,.0f}".format(price_unit)) +'\n'
-		#	elif (price_unit_old < price_unit):
-		#		message_body += 'PLIST UP From '+ str("{:,.0f}".format(price_unit_old))+' to '+str("{:,.0f}".format(price_unit)) +'\n'
-
-		#	if (discount_string_old > discount_string):
-		#		message_body += 'DISC DOWN From '+ str(discount_string_old)+' to '+ str(discount_string) +'\n'
-		#	elif (discount_string_old < discount_string):
-		#		message_body += 'DISC UP From '+ str(discount_string_old)+' to '+ str(discount_string) +'\n'
-
-		#	if (price_unit_nett_old > price_unit_nett):
-		#		message_body += 'NETT DOWN From '+ str("{:,.0f}".format(price_unit_nett_old))+' to '+str("{:,.0f}".format(price_unit_nett)) +'\n'			
-		#	elif (price_unit_nett_old < price_unit_nett):
-		#		message_body += 'NETT UP From '+ str("{:,.0f}".format(price_unit_nett_old))+' to '+str("{:,.0f}".format(price_unit_nett)) +'\n'	
-		#	else:
-		#		message_body += 'NETT : '+ str("{:,.0f}".format(price_unit_nett_old)) +'\n'	
-				
-		#	account_invoice_obj = self.pool.get('account.invoice')
-		#	account_invoice = account_invoice_obj.browse(cr, uid, vals['invoice_id'])
-		#	supplier_name = account_invoice.partner_id.display_name
-
-		#	message_body += 'SELL PRICE:'+str("{:,.0f}".format(sell_price_unit)) +'\n'+'Supplier:'+supplier_name
-		
-		#	message_title = str(vals['name'])
+			line_str += 'SELL PRICE:'+str("{:,.0f}".format(sell_price_unit)) +'\n'+'Supplier:'+partner_name
+			message_body += line_str
 
 			message_title = str(name)
 		
@@ -349,6 +315,7 @@ class account_invoice_line(osv.osv):
 				'category':'INVOICE',
 				'sound_idx':PURCHASE_SOUND_IDX,
 				'alert' : '!!!!!!!',
+				'lines' : line_str,
 				}
 
 			self.pool.get('tbvip.fcm_notif').send_notification(cr,uid,message_title,message_body,context=context)
