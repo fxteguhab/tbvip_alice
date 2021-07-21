d9d0dee1dc68a2fbbdf53d2b4088f3273f8ea17f
fxteguhab <31503256+fxteguhab@users.noreply.github.com>
2021-07-21 20:29:29 +0700

    Update tokopedia.py

--- tokopedia.py
+++ tokopedia.py
@@ -112,7 +112,7 @@ class tokopedia_connector(osv.osv):
 				#_logger.info('SKU : %s',str(product_sku))
 				#_logger.info('response : %s',str(response))
 				if response["data"]:
-					_logger.info('tokopedia response : %s',str(response["data"]["succeed_rows"]))
+					_logger.info('tokopedia response : %s',str(response["data"]))
 					return response["data"]["succeed_rows"]
 				else:
 					_logger.info('update stock @TOKOPEDIA failed for item_sku : %s',str(product_sku))
@@ -131,7 +131,7 @@ class tokopedia_connector(osv.osv):
 			if (STORE_ID != '0'):
 				response = self._call_api(PRODUCT_STOCK_URL,APP_ID+'/price/update?shop_id='+STORE_ID, params=json.dumps(data), method="POST",access_token=token)
 				if response["data"]:
-					_logger.info('tokopedia response : %s',str(response["data"]["succeed_rows"]))
+					_logger.info('tokopedia response : %s',str(response["data"]))
 					return response["data"]["succeed_rows"]
 				else:
 					_logger.info('update price @TOKOPEDIA failed for item_sku : %s',str(product_sku))
